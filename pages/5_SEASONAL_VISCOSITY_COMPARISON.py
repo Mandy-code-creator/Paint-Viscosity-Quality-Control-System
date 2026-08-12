5a0b40646f33d90386a6069c0bd5fb8a143e8bbf
Mandy-code-creator <i109143111@nkust.edu.tw>
2026-08-12 15:16:05 +0800

    Update 5_SEASONAL_VISCOSITY_COMPARISON.py

--- pages/5_SEASONAL_VISCOSITY_COMPARISON.py
+++ pages/5_SEASONAL_VISCOSITY_COMPARISON.py
@@ -2240,7 +2240,7 @@ fig_overview.update_xaxes(
     ticks="outside",
     ticklen=6,
     tickfont=dict(
-        size=15,
+        size=18,
         color="#000000",
     ),
 )
@@ -2253,7 +2253,7 @@ fig_overview.update_yaxes(
     ticks="outside",
     ticklen=5,
     tickfont=dict(
-        size=13,
+        size=15,
         color="#000000",
     ),
     title_font=dict(
